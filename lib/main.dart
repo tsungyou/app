93d2b0425e46a032f74297995e89ebf43c675703
Lee Jimmy <mini@Lees-Mac-mini.local>
2024-12-24 17:17:02 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:test_empty_1/login/login.dart';
 import "package:test_empty_1/login/login.dart";
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: "test",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-    home: const Login(),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: true, home: Login(),));
 }
--- lib/strategies/intraday.dart
+++ lib/strategies/intraday.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:test_empty_1/config.dart';
 class Intraday extends StatelessWidget {
   const Intraday({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text("1234"),
     );
   }
